c2310b7cdd3cf8494814898c1b0f092c0befda5a
carboniti07 <carbonimatheus2@gmail.com>
2026-05-14 16:05:16 -0300

    Corrige CSS Tailwind no build Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [tsconfigPaths(), tailwindcss(), react()],
   server: {
     host: "0.0.0.0",
     port: 8080,
